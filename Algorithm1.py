00f71a70263af62e3c66c977d438d5aac55acb0a
Richard <149290741+rtledev@users.noreply.github.com>
2026-03-22 20:40:20 -0700

    Name updates and comments added

--- Algorithm1.py
+++ Algorithm1.py
@@ -1,3 +1,12 @@
+#Richard Le
+#Richard.le@csu.fullerton.edu
+#Marco Chavez
+#marco_chavez@csu.fullerton.edu
+#Arman Madatyan 
+#armanmadatyan@csu.fullerton.edu
+#Jeremy Mejia
+#fr.jeremy@csu.fullerton.edu
+
 import heapq
 
 class Grid:
--- Algorithm2.py
+++ Algorithm2.py
@@ -1,28 +1,11 @@
-#Pseudo 
-Algorithm NumRescueBoats(people, limit)
-
-Input:
-    people = array of people's weights
-    limit = maximum weight a boat can carry
-
-Output:
-    Minimum number of boats needed
-
-1. Sort people in nondecreasing order
-2. Set left = 0
-3. Set right = length of people - 1
-4. Set boats = 0
-
-5. While left <= right:
-       If people[left] + people[right] <= limit:
-            left = left + 1
-            right = right - 1
-       Else:
-            right = right - 1
-
-       boats = boats + 1
-
-6. Return boats
+#Richard Le
+#Richard.le@csu.fullerton.edu
+#Marco Chavez
+#marco_chavez@csu.fullerton.edu
+#Arman Madatyan 
+#armanmadatyan@csu.fullerton.edu
+#Jeremy Mejia
+#fr.jeremy@csu.fullerton.edu
 
 #Python 
 # CPSC 335 - Project 2
