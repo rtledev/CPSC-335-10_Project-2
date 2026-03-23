afa57b035ad1316b874d6134d9c549dfda351717
Marco Chavez <markachavez2003@gmail.com>
2026-03-22 18:00:59 -0700

    Updated comments

--- Algorithm1.py
+++ Algorithm1.py
@@ -12,6 +12,7 @@ class Grid:
         movement_dir =[(-1,0),(1,0),(0,-1),(0,1)] 
         stored_values = []
         for dr, dc in movement_dir:
+            #calculates position
             new_r = r + dr
             new_c = c + dc
             
@@ -20,30 +21,40 @@ class Grid:
                 #Determine if movement is allowed 0 means yes
                 if self.grid[new_r][new_c] == 0:
                     stored_values.append((new_r, new_c))
+
         return stored_values
 
     def djikstra_alg(self,start, target):
         heap =[]
         dist ={}
 
+        #starting position with a stance of 0
         heapq.heappush(heap, (0, start[0], start[1]))
         dist[start] = 0
 
         while heap:
+            #d = current distance
+            # r, c represents current position
             d,r,c = heapq.heappop(heap)
+
             #target reached
             if (r,c) == target:
                     return d
-            #grid traversal
+            
+            #grid traversal using validation to determine correct path
             for i in self.movement_validation(r,c):
                 new_r, new_c = i
                 distance = d+1
-                if (new_r, new_c) not in dist or distance < dist[(new_r, new_c)]:
-                    dist[(new_r, new_c)] = distance
+
+                if (new_r, new_c) not in dist or distance < dist[(new_r, new_c)]: #Determine if new node is seen or shorter path found
+                    dist[(new_r, new_c)] = distance #current best path
                     heapq.heappush(heap, (distance, new_r, new_c))
         
         #heap empty
         return-1        
+
+start = (0,0)
+target = (5,5)
 
 input1 = [
     [0,0,1,0,0,0],
@@ -54,8 +65,6 @@ input1 = [
     [1,1,0,0,0,0]
 ]
 grid_input = Grid(input1)
-start = (0,0)
-target = (5,5)
 
 input2 = [
     [0,0,0,1,0,0],
@@ -67,8 +76,7 @@ input2 = [
 ]
 
 grid_input2 = Grid(input2)
-start = (0,0)
-target =(5,5)
+
 
 print(grid_input.djikstra_alg(start, target))
 print(grid_input2.djikstra_alg(start,target))
